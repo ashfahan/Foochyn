df5f572a859d84b5faa5172eb608ee8d2bd63b5f
Ashfahan <work@ashfahan.com>
2020-02-11 23:15:40 +0500

    Create contactus.php

---
+++ contactus.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta name="format-detection" content="telephone=no" />
+    <meta name="description" content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum perspiciatis rem praesentium eaque mollitia minima corporis fugit accusantium voluptas ducimus vero itaque voluptatibus maiores laboriosam, dolor id esse. Est, odit?" />
+    <title>FOoChyn</title>
+    <link type="image/png" href="images/favicon.png" rel="icon" />
+    <link href="index.html" rel="canonical" />
+    <link href="./scss/temp/fluid.css" rel="stylesheet" />
+    <link href="./scss/temp/base.css" rel="stylesheet" />
+    <link href="./fonts/Quicksand/fonts.css" rel="stylesheet" />
+    <link href="./vendors/remixicon/remixicon.css" rel="stylesheet" />
+  </head>
+
+  <body>
+    <?php include 'header.php';?>
+
+    <main class="lyt-main container py-2">
+
+      <img class="img" src="./images/index/background.jpg" alt="">
+      <section class="p-2 bg-white txt-justify">
+        <h1 class="txt-center">Contact Us</h1>
+        <p class="mb-1rem">You can reach us at <b>contact@FOoChyn.com</b></p>
+        <p>For booking related queries/issues, kindly call our 24 hours concierge at 7861004444 or mail to us at <a href="mailto:booking@FOoChyn.com"><b>booking@FOoChyn.com</b></a></p>
+        <p><b>For Dubai Customers</b></p>
+        <p>Name of the company : FOoChyn DMCC</p>
+        <p>E-mail address : dubai-support@FOoChyn.com</p>
+        <p>Phone number :&nbsp; +91 7861004444</p>
+        <p>Office Address : </p>
+        <p class="ml-1rem">Unit No: 2392</p>
+        <p class="ml-1rem">DMCC Business Centre</p>
+        <p class="ml-1rem">Level No 1</p>
+        <p class="ml-1rem">Jewellery &amp; Gemplex 3</p>
+        <p class="ml-1rem">Dubai</p>
+        <p class="ml-1rem">United Arab Emirates</p>
+      </section>
+
+    </main>
+
+    <?php include 'footer.php';?>
+  </body>
+  <script src="./js/base.js"></script>
+
+</html>

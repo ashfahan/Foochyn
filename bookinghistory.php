f5e1304aa28315f6be8bba77a9c66f70e947ac56
Ashfahan <work@ashfahan.com>
2020-02-21 04:30:15 +0500

    :up: d

---
+++ bookinghistory.php
@@ -0,0 +1,331 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0" />
+    <link type="image/png" href="images/favicon.png" rel="icon" />
+    <meta name="format-detection" content="telephone=no" />
+    <meta charset="utf-8" />
+    <title>FOoChyn</title>
+    <meta name="description" content="description" />
+    <link href="../index.html" rel="canonical" />
+    <link href="../scss/temp/addresturant.css" rel="stylesheet" />
+  </head>
+
+  <body>
+
+    <div class="grid has-gap-0">
+      <div class="column w-5">
+        <?php include './sidebar.php';?>
+      </div>
+
+      <div class="column">
+        <?php include './header.php';?>
+
+        <div class="px-2">
+          <ol class="breadcrumb column w-24 w-20@sm my-7px">
+            <h2 class="breadcrumb-item mb-0" data-separator="/">Dashboard</h2>
+            <li class="breadcrumb-item" data-separator="/" aria-current="page">Add Restaurant</li>
+          </ol>
+
+          <main class="lyt-main mb-1rem my-1rem bg-white is-round shadow p-2 w-24">
+            <div class="flex justify-between">
+              <h5>Review</h5>
+              <div>Review of restaurant</div>
+            </div>
+
+            <div class="my-1rem has-table-responsive">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th>User Name</th>
+                    <th>Review</th>
+                    <th>Review Date</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  <tr>
+                    <td>AlexanderTyler</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>7/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>AmanyaraRestaurant</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>6/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>AwadhPatel</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>6/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>BaonecciRistorante</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>6/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>ChristinAdkins</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>7/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>CK Restaurant</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>6/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>DipakLadumor</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td></td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>divyladumor</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>10/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>eleaelea</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>6/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>GaryJones</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>Active</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>Great HouseRestaurant</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>6/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>HAVEN Riverfront Restaurant and Bar</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>6/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>LeucaRestaurant</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>6/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>Limewood Restaurant</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>6/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>MukeshAhir</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>10/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>NewRestaurant</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>12/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>NiraliPatel</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>10/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>Northwest PointResort Restaurant</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>6/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>OtherMama</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>7/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>ParmarParmar</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>14/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>ParmarParmar</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td></td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>ParmarParmar</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td></td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>PQRASD</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>10/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>pqrxyz</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>10/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>RonakParmar</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>14/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>RonakParmar</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>14/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>RonakParmar</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td></td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>RonakParmar</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td></td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>RonakParmar</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td></td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>RonakParmar</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td></td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>RonakParmar</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>13/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>RonakParmar</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>Active</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>RonakParmar</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>14/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>SandboxRestaurant</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>6/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>scvsscvs</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>Active</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>SharkRestaurant</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>12/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>ShreeMehfil</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>12/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>sureshladumor</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>12/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>Table Restaurant</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td></td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>The DeckRestaurant</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>6/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>umeshladumor</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td></td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>UmeshLadumor</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>7/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>Waterbar</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>6/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>YasGohil</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>7/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                  <tr>
+                    <td>Zest at Wymara ResortRestaurant</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit.</td>
+                    <td>6/2/2020</td>
+                    <td><a class="ml-1rem" href="#"><i class="icon ri-delete-bin-2-line"></i> Remove</a></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+          </main>
+        </div>
+      </div>
+    </div>
+
+    <?php include './footer.php';?>
+  </body>
+
+</html>
---
+++ sidebar.php
@@ -0,0 +1,13 @@
+<aside class="menu is-primary clr-white bg-dark shadow h-100 h-min-100vh">
+  <div href="#" class="txt-center py-1rem h2 mb-0">User Dashboard</div>
+  <div href="#" class="txt-center bg-black py-1rem">Menu Nagivation</div>
+  <a href="#" class="menu-item py-1rem">
+    <i class="icon is-lg ri-article-line"></i>&nbsp;<span>Account Setting</span>
+  </a>
+  <a href="#" class="menu-item py-1rem">
+    <i class="icon is-lg ri-building-2-line"></i>&nbsp;<span>Notification</span>
+  </a>
+  <a href="#" class="menu-item py-1rem">
+    <i class="icon is-lg ri-building-2-line"></i>&nbsp;<span>Booking history</span>
+  </a>
+</aside>

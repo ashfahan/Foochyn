bffbb9a9b3055b12ee0e4448c4228560f41afe54
Ashfahan <work@ashfahan.com>
2020-02-24 16:35:26 +0500

    :up:

---
+++ user/footer.php
@@ -0,0 +1,42 @@
+<footer class="bg-dark p-2 clr-white grid txt-center">
+  <div class="column w-8">
+    <h3>Diners</h3>
+    <div>Join more than 440,000 diners</div>
+    <div>who discover new restaurants with FOoChyn</div>
+    <div class="mt-1rem"><button class="btn is-sld is-light is-lg">Learn More</button></div>
+  </div>
+  <div class="column w-8">
+    <h3>Restaurants</h3>
+    <div>Join more than 1,300 resturants</div>
+    <div>who promote their restaurant with FOoChyn</div>
+    <div class="mt-1rem"><button class="btn is-sld is-light is-lg">Learn More</button></div>
+  </div>
+  <div class="column w-8">
+    <h3>Follow Us</h3>
+    <div>Join more than 180,000 fans</div>
+    <div>who rave absolute FOoChyn on social</div>
+    <div class="mt-1rem">
+      <a class="clr-white" href="#"><i class="icon title is-sm ri-facebook-box-line"></i></a>
+      <a class="clr-white" href="#"><i class="icon title is-sm ri-instagram-line"></i></a>
+    </div>
+  </div>
+  <div class=" column w-15 mx-auto mt-2 mb-7px flex justify-between">
+    <a class="clr-white" href="#">About us</a>
+    <a class="clr-white" href="#">FAQ</a>
+    <a class="clr-white" href="#">Contact US</a>
+    <a class="clr-white" href="#">Partner With us</a>
+    <a class="clr-white" href="#">Terms & Conditions</a>
+    <a class="clr-white" href="#">Privacy Policy</a>
+    <a class="clr-white" href="#">Restaurateurs</a>
+  </div>
+  <div class="border-bottom w-24"></div>
+  <div class="container column w-24">© First Table (Australia) Pty Ltd 2014 - 2020. All rights reserved. The First Table logo and name are trademarks of First Table Limited.</div>
+</footer>
+
+<script src="../vendors/jquery-3.4.1.min.js"></script>
+<script src="../vendors/jquery-ui/jquery-ui.min.js"></script>
+<script src="../vendors/choices/public/assets/scripts/choices.js"></script>
+<link href="../vendors/DataTables/datatables.css" rel="stylesheet">
+<script src="../vendors/DataTables/datatables.js"></script>
+<script src="../js/base.js"></script>
+<script src="../js/MULTISTEPFORM.js"></script>
---
+++ user/header.php
@@ -0,0 +1,20 @@
+<link href="../scss/temp/fluid.css" rel="stylesheet" />
+<link href="../scss/temp/base.css" rel="stylesheet" />
+<link href="../fonts/Quicksand/fonts.css" rel="stylesheet" />
+<link href="../vendors/remixicon/remixicon.css" rel="stylesheet" />
+<link href="../vendors/slick/slick.css" rel="stylesheet" />
+<link href="../vendors/slick/slick-theme.css" rel="stylesheet" />
+<link href="../vendors/choices/public/assets/styles/choices.css" rel="stylesheet" />
+
+<header class="lyt-header shadow-sm bg-white txt-center grid border-top-none">
+  <button class="btn"><span class="icon is-lg"><i class="ri-menu-line"></i></span></button>
+  <div class="column w-4 txt-center border-right pr-7px">
+    <a href="."> <img class="img w-17" src="../images/logo.svg" alt="EazyDiner"> </a>
+  </div>
+
+  <div class="column txt-right">
+    <div class="icon is-lg mx-1rem"><i class="ri-settings-line"></i> </div>
+    <div class="icon is-lg mx-1rem"><i class="ri-notification-2-line"></i></div>
+    <div class="icon mx-1rem border border-wide border-dark is-circle img-bg" style="padding: 1.5rem; background-image: url('../images/profile.jpg');"><i class="z--10 ri-user-line"></i></div>
+  </div>
+</header>

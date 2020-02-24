3a68084643629851ed8ef3ed09757e417d2618fc
Ashfahan <work@ashfahan.com>
2020-02-24 10:37:03 +0500

    :fire:

--- header.php
+++ header.php
@@ -10,29 +10,7 @@
   <div class="column w-4 txt-center border-right pr-7px">
     <a href="."> <img class="img w-17" src="./images/logo.svg" alt="EazyDiner"> </a>
   </div>
-  <div class="column w-4 pr-7px">
-    <select class="input choices w-16 is-unstyle">
-      <option value="" >City</option>
-      <optgroup label="City 1">
-        <option>Area 1</option>
-        <option>Area 2</option>
-        <option>Area 3</option>
-        <option>Area 4</option>
-        <option>Area 5</option>
-      </optgroup>
-
-      <optgroup label="City 2">
-        <option>Area 6</option>
-        <option>Area 7</option>
-        <option>Area 8</option>
-        <option>Area 9</option>
-        <option>Area 10</option>
-        <option>Area 11</option>
-      </optgroup>
-    </select>
-  </div>
-  <div class="column w-8 pr-7px"></div>
-  <a class="txt-decor-none font-bold border-right clr-primary w-4 pr-7px" href="#"><i class="ri-phone-line icon mr-7px"></i>786 100 4444</a>
+  <a class="off-12 txt-decor-none font-bold border-right clr-primary w-4 pr-7px" href="#"><i class="ri-phone-line icon mr-7px"></i>786 100 4444</a>
 
   <div class="column w-4 flex justify-between align-middle pl-7px">
     <a class="btn" href="#">Help</a>

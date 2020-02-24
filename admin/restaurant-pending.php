884736deb7d02760df6919efca07929ee6b7864b
Ashfahan <work@ashfahan.com>
2020-02-24 17:55:22 +0500

    Update restaurant-pending.php

--- admin/restaurant-pending.php
+++ admin/restaurant-pending.php
@@ -9,7 +9,7 @@
     <title>FOoChyn</title>
     <meta name="description" content="description" />
     <link href="../index.html" rel="canonical" />
-    </head>
+  </head>
 
   <body>
     <div class="grid has-gap-0">
@@ -26,41 +26,50 @@
             <li class="breadcrumb-item" data-separator="/" aria-current="page">Add Restaurant</li>
           </ol>
 
-        <main class="lyt-main mb-1rem">
-          <form>
-            <div class="bg-white shadow border p-2 is-round">
-              <h5>Pending Restaurants</h5>
-              <table class="table">
-                <th>Name</th>
-                <th>Date</th>
-                <th>Status</th>
-                <th>Action</th>
-                <tr>
-                  <td><a href="#">xyz</a></td>
-                  <td>date</td>
-                  <td>Pending</td>
-                  <td><button class="btn is-sld is-primary"> <i class="icon  ri-check-double-fill"></i> Approve</button></td>
-                </tr>
-                <tr>
-                  <td><a href="#">xyz</a></td>
-                  <td>date</td>
-                  <td>Pending</td>
-                  <td><button class="btn is-sld is-primary"> <i class="icon  ri-check-double-fill"></i> Approve</button></td>
-                </tr>
-                <tr>
-                  <td><a href="#">xyz</a></td>
-                  <td>date</td>
-                  <td>Pending</td>
-                  <td><button class="btn is-sld is-primary"> <i class="icon  ri-check-double-fill"></i> Approve</button></td>
-                </tr>
-              </table>
-            </div>
-          </form>
-        </main>
+          <main class="lyt-main mb-1rem">
+            <form>
+              <div class="bg-white shadow border p-2 is-round">
+                <h5>Pending Restaurants</h5>
+                <table class="table">
+                  <th>Name</th>
+                  <th>Date</th>
+                  <th>Status</th>
+                  <th>Action</th>
+                  <tr>
+                    <td><a href="#">xyz</a></td>
+                    <td>date</td>
+                    <td>Pending</td>
+                    <td>
+                      <button class="btn is-sld is-primary"> <i class="icon ri-eye-fill"></i> View Detail</button>
+                      <button class="btn is-sld is-primary"> <i class="icon ri-check-double-fill"></i> Approve</button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td><a href="#">xyz</a></td>
+                    <td>date</td>
+                    <td>Pending</td>
+                    <td>
+                      <button class="btn is-sld is-primary"> <i class="icon ri-eye-fill"></i> View Detail</button>
+                      <button class="btn is-sld is-primary"> <i class="icon ri-check-double-fill"></i> Approve</button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td><a href="#">xyz</a></td>
+                    <td>date</td>
+                    <td>Pending</td>
+                    <td>
+                      <button class="btn is-sld is-primary"> <i class="icon ri-eye-fill"></i> View Detail</button>
+                      <button class="btn is-sld is-primary"> <i class="icon ri-check-double-fill"></i> Approve</button>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+            </form>
+          </main>
+        </div>
       </div>
-    </div>
 
-    <?php include './footer.php';?>
+      <?php include './footer.php';?>
 
   </body>
 

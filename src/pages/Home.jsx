3c3af876f2a655fe16808a137e865a75d9812503
Ashfahan <work@ashfahan.com>
2020-03-27 04:18:02 +0500

    basic homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import { Button } from "antd";
+import TitleBg from "../images/home-title-bg.jpg";
 
 class Home extends Component {
   render() {
     return (
-      <div className="my-2">
-        <div className="mb-4">This is Home</div>
+      <div className="">
+        <section className="flex justify-center align-middle h-min-100vh bg-img" style={{ backgroundImage: `url(${TitleBg})` }}>
+          sad
+        </section>
+        <div className="h-min-100vh mb-4">This is Home</div>
+        <div className="h-min-100vh mb-4">This is Home</div>
+        <div className="h-min-100vh mb-4">This is Home</div>
+        <div className="h-min-100vh mb-4">This is Home</div>
         <NavLink to="/xyz">
           <Button type="primary">Goto xyz ?</Button>
         </NavLink>

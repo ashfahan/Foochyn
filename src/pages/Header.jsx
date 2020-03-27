ae63d6302b3de3c50c0bedbbe6fbf6fee9a6a58d
Ashfahan <work@ashfahan.com>
2020-03-28 04:34:30 +0500

    :up: Add header links and fix classname warning

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Headroom from "headroom.js";
+import { NavLink } from "react-router-dom";
 import _ from "lodash";
 
 export default class Header extends Component {
@@ -67,29 +68,32 @@ export default class Header extends Component {
   render() {
     return (
       <header ref={this.ref} className={`ant-layout-header lyt-header grid ${this.props.className}`}>
-        <div className="column w-4">Foochyn</div>
-        <div className="column w-13">{/* <input type="search" className="input" /> */}</div>
-        <div className="column w-5 txt-right">
-          <button className="btn is-out is-primary mr-2">Login</button>
-          <button className="btn is-sld is-primary mr-2">Sign Up</button>
-          <div class="dropdown on-hover">
-            <button class="btn is-sld is-primary dropdown-trigger">Language</button>
-            <div class="menu is-primary right-0 clr-body txt-left">
-              <button class="menu-item">Mandarin</button>
-              <button class="menu-item">Spanish</button>
-              <button class="menu-item">English</button>
-              <button class="menu-item">Urdu</button>
-              <button class="menu-item">Arabic</button>
-              <button class="menu-item">Portuguese</button>
-              <button class="menu-item">Bengali</button>
-              <button class="menu-item">Russian</button>
-              <button class="menu-item">Japanese</button>
-              <button class="menu-item">Punjabi</button>
-              <button class="menu-item">German</button>
-              <button class="menu-item">Javanese</button>
-              <button class="menu-item">Wu</button>
-              <button class="menu-item">Indonesian</button>
-              <button class="menu-item">Telugu</button>
+        <div className="column w-4 font-bold is-lg">Foochyn</div>
+        <div className="column w-20 txt-right">
+          <NavLink to="/search">
+            <button className="btn is-out is-primary mr-7px">Find Table</button>
+          </NavLink>
+          <NavLink to="/signup">
+            <button className="btn is-sld is-primary mr-7px">Join Us</button>
+          </NavLink>
+          <div className="dropdown on-hover">
+            <button className="btn is-sld is-primary dropdown-trigger">Language</button>
+            <div className="menu is-primary right-0 clr-body txt-left">
+              <button className="menu-item">Mandarin</button>
+              <button className="menu-item">Spanish</button>
+              <button className="menu-item">English</button>
+              <button className="menu-item">Urdu</button>
+              <button className="menu-item">Arabic</button>
+              <button className="menu-item">Portuguese</button>
+              <button className="menu-item">Bengali</button>
+              <button className="menu-item">Russian</button>
+              <button className="menu-item">Japanese</button>
+              <button className="menu-item">Punjabi</button>
+              <button className="menu-item">German</button>
+              <button className="menu-item">Javanese</button>
+              <button className="menu-item">Wu</button>
+              <button className="menu-item">Indonesian</button>
+              <button className="menu-item">Telugu</button>
             </div>
           </div>
         </div>

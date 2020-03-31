3d5a084721d51bd6ef2d992784dd4f878bc6c1cf
Ashfahan <work@ashfahan.com>
2020-03-31 19:19:32 +0500

    :up: :art: only wrap card details with padding

--- src/components/ResturantCard.tsx
+++ src/components/ResturantCard.tsx
@@ -15,19 +15,21 @@ interface props {
 export default class ResturantCard extends Component<props> {
   render() {
     return (
-      <div className="bg-white p-7px">
-        <div className="bg-primary img-bg is-round-sm txt-right relative" style={{ minHeight: "200px", backgroundImage: `url(${this.props.image})` }}>
+      <div className="bg-white is-round-sm overflow-hidden">
+        <div className="bg-primary img-bg txt-right relative" style={{ minHeight: "200px", backgroundImage: `url(${this.props.image})` }}>
           <i className={`absolute ri-2x txt-shadow-round right-0 py-7px px-1rem ${this.props.favorite ? "ri-heart-fill clr-primary" : "ri-heart-add-line clr-white"}`}></i>
         </div>
-        <h5 className="my-7px">{this.props.title}</h5>
-        <div className="flex align-baseline">
-          <span className="clr-primary">{this.props.rating}</span>
-          <Rate className="clr-primary mx-7px is-md" allowHalf value={this.props.rating} disabled />
-          <span className="clr-disabled">({this.props.reviews})</span>
+        <div className="p-7px">
+          <h5 className="my-7px">{this.props.title}</h5>
+          <div className="flex align-baseline">
+            <span className="clr-primary">{this.props.rating}</span>
+            <Rate className="clr-primary mx-7px is-md" allowHalf value={this.props.rating} disabled />
+            <span className="clr-disabled">({this.props.reviews})</span>
+          </div>
+          <div className="clr-disabled">{this.format(this.props.location)}</div>
+          <div className="clr-disabled">{this.format(this.props.cuisines)}</div>
+          <div className="clr-disabled">{this.format(this.props.pricerange)} PKR Approx Per User</div>
         </div>
-        <div className="clr-disabled">{this.format(this.props.location)}</div>
-        <div className="clr-disabled">{this.format(this.props.cuisines)}</div>
-        <div className="clr-disabled">{this.format(this.props.pricerange)} PKR Approx Per User</div>
       </div>
     );
   }

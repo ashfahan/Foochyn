7c670d5495d62484cba2c8124265c53b207db477
Ashfahan <work@ashfahan.com>
2020-04-04 04:28:16 +0500

    :heavy_plus_sign: add items in restuarnt about

--- src/pages/Resturant.tsx
+++ src/pages/Resturant.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 import "../scss/home.scss";
-import { Rate, Tabs } from "antd";
+import { Rate, Tabs, Tooltip } from "antd";
 import Expend from "../components/Expend";
 
 interface props {
@@ -28,7 +28,7 @@ class Resturant extends Component<props> {
     reviews: 0,
     location: ["South Padre Island", "TX"],
     cuisines: ["Chinese", "Italian", "Japanese"],
-    pricerange: [0],
+    pricerange: [1000, 5000],
     diningReview: 3.8,
     diningReviewCount: 1455,
     deliveryReview: 4.5,
@@ -106,16 +106,32 @@ class Resturant extends Component<props> {
             }
           >
             <Tabs.TabPane tab="Overview" key="1">
-              <div className="px-4 py-2 container">
-                <section>
-                  <h3 className="txt-cap mb-0">Known For</h3>
-                  <hr className="clr-primary w-1 mt-7px mb-1rem o-100" style={{ height: "3px" }} />
-                  <p>A Cisternino, all'interno di una caratteristica aia immersa nella Valle d'Itria, fa capolino questo intimo complesso di trulli risalenti al 1800, ristrutturati secondo la tradizione. Un posto magico dove trascorrere una vacanza all'insegna del relax .</p>
-                  <Expend showText="Read More" hideText="Read Less">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam, reiciendis ut eveniet autem cupiditate facere perferendis beatae officiis cumque esse laborum porro ad alias? Ipsum repellat soluta assumenda! Suscipit, ipsum.
-                  </Expend>
-                </section>
-              </div>
+              <section className="px-4 py-1rem container">
+                <h3 className="txt-cap mb-0">Known For</h3>
+                <hr className="clr-primary w-1 mt-7px mb-1rem o-100" style={{ height: "3px" }} />
+                <div>A Cisternino, all'interno di una caratteristica aia immersa nella Valle d'Itria, fa capolino questo intimo complesso di trulli risalenti al 1800, ristrutturati secondo la tradizione. Un posto magico dove trascorrere una vacanza all'insegna del relax .</div>
+                <Expend showText="Read More" hideText="Read Less">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam, reiciendis ut eveniet autem cupiditate facere perferendis beatae officiis cumque esse laborum porro ad alias? Ipsum repellat soluta assumenda! Suscipit, ipsum.
+                </Expend>
+              </section>
+
+              <section className="px-4 py-1rem container">
+                <h3 className="txt-cap mb-0">Cuisines</h3>
+                <hr className="clr-primary w-1 mt-7px mb-1rem o-100" style={{ height: "3px" }} />
+                <button className="btn is-out is-pill mr-7px">North Indian</button>
+                <button className="btn is-out is-pill mr-7px">Chinese</button>
+              </section>
+
+              <section className="px-4 py-1rem container">
+                <h3 className="txt-cap mb-0">Average Cost</h3>
+                <hr className="clr-primary w-1 mt-7px mb-1rem o-100" style={{ height: "3px" }} />
+                <div>{this.formatArray(this.props.pricerange, " - ")} PKR for two people (approx.)</div>
+                <div className="clr-disabled o-75">Exclusive of applicable taxes and charges, if any</div>
+
+                <Tooltip placement="rightTop" title="The cost for two is computed as follows: Average of 2 appetizers + 2 mains + 2 beverages + 1 dessert. The actual cost you incur at a restaurant might change depending on your appetite, or with changes in restaurant menu prices">
+                  <div className="clr-disabled o-50 border-none border-bottom border-dashed inline">How do we calculate cost for two?</div>
+                </Tooltip>
+              </section>
             </Tabs.TabPane>
             <Tabs.TabPane tab="Gallary" key="2">
               <div className="px-4 py-2 container"></div>

7c00c7f1312bebb87f46034459eb8e34578b697c
Ashfahan <work@ashfahan.com>
2020-04-06 03:45:44 +0500

    :up: :art: ues > for breakpoints above and < for breakpoints below

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -31,19 +31,19 @@ class Home extends Component {
         <section className="mb-2 clr-white flex justify-center align-middle bg-img" style={{ minHeight: "400px", backgroundImage: `linear-gradient(to bottom, hsla(0, 0%, 0%, 0.3), hsla(0, 0%, 0%, 0.5)), url(${TitleBg})` }}>
           <div className="w-24 p-6">
             <h1 className="txt-center txt-cap">Find your table for any occasion</h1>
-            <div className="grid block flex@lg">
-              <div className="column group w-11@lg block flex@sm">
-                <div className="control has-icon-left is-lg my-0@sm">
+            <div className="grid block flex>lg">
+              <div className="column group w-11>lg block flex>sm">
+                <div className="control has-icon-left is-lg my-0>sm">
                   <i className="clr-body z-10 icon is-left ri-calendar-line"></i>
-                  <DatePicker suffixIcon={<i className="clr-body icon ri-arrow-down-s-fill" />} placeholder="Date" allowClear={false} defaultValue={moment(moment.now())} size="large" className="input is-round-sm@sm" format="D/M/YYYY" />
+                  <DatePicker suffixIcon={<i className="clr-body icon ri-arrow-down-s-fill" />} placeholder="Date" allowClear={false} defaultValue={moment(moment.now())} size="large" className="input is-round-sm<sm" format="D/M/YYYY" />
                 </div>
-                <div className="control has-icon-left is-lg my-7px my-0@sm">
+                <div className="control has-icon-left is-lg my-7px my-0>sm">
                   <i className="clr-body z-10 icon is-left ri-calendar-line"></i>
-                  <TimePicker suffixIcon={<i className="clr-body icon ri-arrow-down-s-fill" />} placeholder="Time" allowClear={false} defaultValue={moment(moment.now()).add(1, "hour")} size="large" className="input is-round-sm@sm" format="h:mm a" minuteStep={15} />
+                  <TimePicker suffixIcon={<i className="clr-body icon ri-arrow-down-s-fill" />} placeholder="Time" allowClear={false} defaultValue={moment(moment.now()).add(1, "hour")} size="large" className="input is-round-sm<sm" format="h:mm a" minuteStep={15} />
                 </div>
-                <div className="control has-icon-left is-lg my-0@sm">
+                <div className="control has-icon-left is-lg my-0>sm">
                   <i className="clr-body z-10 icon is-left ri-user-line"></i>
-                  <Select bordered={false} size="large" suffixIcon={<i className="clr-body icon ri-arrow-down-s-fill" />} className="input is-round-sm@sm" showSearch defaultValue="2">
+                  <Select bordered={false} size="large" suffixIcon={<i className="clr-body icon ri-arrow-down-s-fill" />} className="input is-round-sm<sm" showSearch defaultValue="2">
                     <Select.Option value="1">1 People</Select.Option>
                     <Select.Option value="2">2 People</Select.Option>
                     <Select.Option value="3">3 People</Select.Option>
@@ -62,7 +62,7 @@ class Home extends Component {
                   </Select>
                 </div>
               </div>
-              <div className="column w-7@lg">
+              <div className="column w-7>lg">
                 <div className="control has-icon-left is-lg">
                   <i className="clr-body z-10 icon is-left ri-map-pin-line"></i>
                   <Select bordered={false} size="large" suffixIcon={<i className="clr-body icon ri-arrow-down-s-fill" />} className="input" showSearch placeholder="Location, Restaurant, Cuisine">
@@ -95,14 +95,14 @@ class Home extends Component {
                   </Select>
                 </div>
               </div>
-              <div className="column w-5@lg">
+              <div className="column w-5>lg">
                 <button className="btn is-block h-100 is-sld is-primary">Search Table</button>
               </div>
             </div>
           </div>
         </section>
 
-        <section className="p-4 py-2@md container">
+        <section className="p-4 py-2>md container">
           <h2 className="txt-cap mb-0">Popular Offer in {"city"}</h2>
           <hr className="clr-primary w-3 mt-7px mb-1rem o-100" style={{ height: "3px", minWidth: "3rem" }} />
           <Carousel
@@ -170,7 +170,7 @@ class Home extends Component {
           </Carousel>
         </section>
 
-        <section className="p-4 py-2@md container">
+        <section className="p-4 py-2>md container">
           <h2 className="txt-cap mb-0">Featured restaurants in {"city"}</h2>
           <hr className="clr-primary w-3 mt-7px mb-1rem o-100" style={{ height: "3px", minWidth: "3rem" }} />
           <Carousel
@@ -222,57 +222,57 @@ class Home extends Component {
           </Carousel>
         </section>
 
-        <section className="p-4 py-2@md container">
+        <section className="p-4 py-2>md container">
           <h2 className="txt-cap mb-0">Popular Areas in {"city"}</h2>
           <hr className="clr-primary w-3 mt-7px mb-1rem o-100" style={{ height: "3px", minWidth: "3rem" }} />
           <div className="grid has-gap-lg">
-            <div className="column w-6@lg w-12@sm w-24">
+            <div className="column w-6>lg w-12>sm w-24">
               <LocationCard location="Lahore" resurantcount={500} image="https://source.unsplash.com/512x384/?Lahore" />
             </div>
-            <div className="column w-6@lg w-12@sm w-24">
+            <div className="column w-6>lg w-12>sm w-24">
               <LocationCard location="Islamabad" resurantcount={500} image="https://source.unsplash.com/512x384/?Islamabad" />
             </div>
-            <div className="column w-6@lg w-12@sm w-24">
+            <div className="column w-6>lg w-12>sm w-24">
               <LocationCard location="Karachi" resurantcount={500} image="https://source.unsplash.com/512x384/?Karachi" />
             </div>
-            <div className="column w-6@lg w-12@sm w-24">
+            <div className="column w-6>lg w-12>sm w-24">
               <LocationCard location="Peshawar" resurantcount={500} image="https://source.unsplash.com/512x384/?Peshawar" />
             </div>
           </div>
         </section>
 
-        <section className="p-4 py-2@md container">
+        <section className="p-4 py-2>md container">
           <h2 className="txt-cap mb-0">Popular Cuisines</h2>
           <hr className="clr-primary w-3 mt-7px mb-1rem o-100" style={{ height: "3px", minWidth: "3rem" }} />
           <div className="grid has-gap-lg">
-            <div className="column w-6@lg w-12@sm w-24">
+            <div className="column w-6>lg w-12>sm w-24">
               <CuisinesCard image={chinese} name="Chinese" resurantcount={648} />
             </div>
-            <div className="column w-6@lg w-12@sm w-24">
+            <div className="column w-6>lg w-12>sm w-24">
               <CuisinesCard image={european} name="European" resurantcount={562} />
             </div>
-            <div className="column w-6@lg w-12@sm w-24">
+            <div className="column w-6>lg w-12>sm w-24">
               <CuisinesCard image={italian} name="Italian" resurantcount={267} />
             </div>
-            <div className="column w-6@lg w-12@sm w-24">
+            <div className="column w-6>lg w-12>sm w-24">
               <CuisinesCard image={japanese} name="Japanese" resurantcount={94} />
             </div>
-            <div className="column w-6@lg w-12@sm w-24">
+            <div className="column w-6>lg w-12>sm w-24">
               <CuisinesCard image={mediterranean} name="Mediterranean" resurantcount={94} />
             </div>
-            <div className="column w-6@lg w-12@sm w-24">
+            <div className="column w-6>lg w-12>sm w-24">
               <CuisinesCard image={northindian} name="North Indian" resurantcount={267} />
             </div>
-            <div className="column w-6@lg w-12@sm w-24">
+            <div className="column w-6>lg w-12>sm w-24">
               <CuisinesCard image={panasian} name="Pan Asian" resurantcount={562} />
             </div>
-            <div className="column w-6@lg w-12@sm w-24">
+            <div className="column w-6>lg w-12>sm w-24">
               <CuisinesCard image={southindian} name="South Indian" resurantcount={648} />
             </div>
           </div>
         </section>
 
-        <section className="p-4 py-2@md container">
+        <section className="p-4 py-2>md container">
           <h2 className="txt-cap mb-0">Popular restaurants in {"city"}</h2>
           <hr className="clr-primary w-3 mt-7px mb-1rem o-100" style={{ height: "3px", minWidth: "3rem" }} />
           <Carousel
@@ -324,7 +324,7 @@ class Home extends Component {
           </Carousel>
         </section>
 
-        <section className="p-4 py-2@md container">
+        <section className="p-4 py-2>md container">
           <h2 className="txt-cap mb-0">Newly added resturants in {"city"}</h2>
           <hr className="clr-primary w-3 mt-7px mb-1rem o-100" style={{ height: "3px", minWidth: "3rem" }} />
           <Carousel
@@ -376,25 +376,25 @@ class Home extends Component {
           </Carousel>
         </section>
 
-        <section className="p-4 py-4@md bg-primary clr-white">
+        <section className="p-4 py-4>md bg-primary clr-white">
           <h2 className="txt-center txt-cap">Why Book a Restaurant With Foochyn</h2>
           <div className="grid has-gap-lg txt-center">
-            <div className="column p-2 w-6@lg w-12@sm w-24">
+            <div className="column p-2 w-6>lg w-12>sm w-24">
               <div className="ri-search-eye-line ri-8x" />
               <h4 className="mb-6">Discover Restaurants</h4>
               <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem possimus reiciendis, sit dolorum alias expedita praesentium natus dolor</div>
             </div>
-            <div className="column p-2 w-6@lg w-12@sm w-24">
+            <div className="column p-2 w-6>lg w-12>sm w-24">
               <div className="ri-feedback-line ri-8x" />
               <h4 className="mb-6">Restaurants Reviews</h4>
               <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem possimus reiciendis, sit dolorum alias expedita praesentium natus dolor</div>
             </div>
-            <div className="column p-2 w-6@lg w-12@sm w-24">
+            <div className="column p-2 w-6>lg w-12>sm w-24">
               <div className="ri-calendar-check-line ri-8x" />
               <h4 className="mb-6">Quick Booking</h4>
               <div>Instant booking in 20 seconds!. amet consectetur adipisicing elit. Voluptatem possimus reiciendis, sit dolorum alias expedita praesentium natus</div>
             </div>
-            <div className="column p-2 w-6@lg w-12@sm w-24">
+            <div className="column p-2 w-6>lg w-12>sm w-24">
               <div className="ri-refund-line ri-8x" />
               <h4 className="mb-6">Free Cancelation</h4>
               <div>Free cancellation always flexible always free. amet consectetur adipisicing elit. Voluptatem possimus reiciendis, sit dolorum alias expedita natus</div>

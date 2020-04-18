9097a7de32253b350edee8396f8c021633c20e24
Ashfahan <work@ashfahan.com>
2020-04-18 14:10:00 +0500

    :up: Alot of u pdates in resturantwoner dashboard

--- src/pages/ResturantDashboard.tsx
+++ src/pages/ResturantDashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { DatePicker, Tabs } from "antd";
 import moment from "moment";
 
@@ -97,43 +97,90 @@ export default function ResturantDashboard() {
 }
 
 export function Reserve() {
-  const [seated, setSeated] = useState<boolean>(false);
-  const [upcoming, setUpcoming] = useState<boolean>(false);
-  const [reservation, setReservation] = useState<boolean>(false);
-  const [shift, setShift] = useState<boolean>(false);
+  const [searchOption, setSearchOption] = useState<boolean>(false);
+
+  const [status, setStatus] = useState<null | "notconfirmed" | "noanswer" | "confirmed" | "denied" | "canceled" | "noshow" | "arrived" | "late" | "checkdropped" | "finished">(null);
+  const [reservationtype, setReservationtype] = useState<null | "online" | "in-house">(null);
+  const [sort, setSort] = useState<"reservationtimeASC" | "reservationtimeDEC" | "createdtimeASC" | "createdtimeDEC">("reservationtimeDEC");
 
   return (
     <>
       <div className="grid h-100">
         <div className="column bg-white p-0" style={{ flex: "none", width: "400px" }}>
-          <Tabs defaultActiveKey="1" className="expand">
+          <Tabs defaultActiveKey="1" className="expand h-100">
             <Tabs.TabPane tab="Reservations" key="1" style={{ height: 200 }}>
               <div className="group">
-                <label className="control has-icon-left test w-21">
-                  <i className="icon is-lg is-left ri-search-2-line"></i>
-                  <input type="search" className="input is-min bg-hide is-box py-7px" />
+                <label className="control has-icon-left w-21">
+                  <i className="icon is-lg is-left ri-search-2-line" />
+                  <input onFocus={() => setSearchOption(false)} type="search" className="input is-min bg-hide is-box py-7px" />
                 </label>
-                <button className="btn is-box is-out border-none border-left border-bottom">
-                  <i className="after-hide icon ri-2x ri-filter-3-line"></i>
+                <button className="btn is-box is-out border-none border-left border-bottom" onClick={() => setSearchOption(!searchOption)}>
+                  <i className="after-hide icon ri-2x ri-filter-3-line" />
                 </button>
               </div>
-              <div className="p-7px">
-                <label className="control pr-7px pt-7px">
-                  <input type="checkbox" className="checkbox" checked={seated} onChange={(e) => setSeated(e.target.checked)} />
-                  <span className={`btn ${seated ? "is-sld" : "is-out"} is-pill`}>Seated</span>
-                </label>
-                <label className="control pr-7px pt-7px">
-                  <input type="checkbox" className="checkbox" checked={upcoming} onChange={(e) => setUpcoming(e.target.checked)} />
-                  <span className={`btn ${upcoming ? "is-sld" : "is-out"} is-pill`}>Upcoming</span>
-                </label>
-                <label className="control pr-7px pt-7px">
-                  <input type="checkbox" className="checkbox" checked={reservation} onChange={(e) => setReservation(e.target.checked)} />
-                  <span className={`btn ${reservation ? "is-sld" : "is-out"} is-pill`}>Reservation time</span>
-                </label>
-                <label className="control pr-7px pt-7px">
-                  <input type="checkbox" className="checkbox" checked={shift} onChange={(e) => setShift(e.target.checked)} />
-                  <span className={`btn ${shift ? "is-sld" : "is-out"} is-pill`}>Shift</span>
-                </label>
+              <div className="relative bottom-0 left-0 right-0 top-0 flex justify-center h-100">
+                <div className={`bg-white absolute top-0 left-0 ${!searchOption ? "hidden" : ""}`}>
+                  <div className="p-7px">
+                    <h3>Sort By</h3>
+                    <button className={`btn is-pill mt-7px mr-7px ${sort == "reservationtimeASC" || sort == "reservationtimeDEC" ? "is-sld" : "is-out"}`} onClick={() => setSort(sort == "reservationtimeASC" ? "reservationtimeDEC" : "reservationtimeASC")}>
+                      <i className={`icon is-lg ri-arrow-${sort == "reservationtimeASC" ? "up" : "down"}-s-fill`} /> Reservation time
+                    </button>
+                    <button className={`btn is-pill mt-7px mr-7px ${sort == "createdtimeASC" || sort == "createdtimeDEC" ? "is-sld" : "is-out"}`} onClick={() => setSort(sort == "createdtimeASC" ? "createdtimeDEC" : "createdtimeASC")}>
+                      <i className={`icon is-lg ri-arrow-${sort == "createdtimeASC" ? "up" : "down"}-s-fill`} /> Created time
+                    </button>
+                  </div>
+
+                  <div className="p-7px">
+                    <h3>Filter By</h3>
+                    <div className="p-7px">
+                      <h5 className="font-normal">Status</h5>
+                      <div>
+                        <button className={`btn is-pill mt-7px mr-7px ${status == "notconfirmed" ? "is-sld" : "is-out"}`} onClick={() => setStatus(status == "notconfirmed" ? null : "notconfirmed")}>
+                          Not confirmed
+                        </button>
+                        <button className={`btn is-pill mt-7px mr-7px ${status == "noanswer" ? "is-sld" : "is-out"}`} onClick={() => setStatus(status == "noanswer" ? null : "noanswer")}>
+                          No answer
+                        </button>
+                        <button className={`btn is-pill mt-7px mr-7px ${status == "confirmed" ? "is-sld" : "is-out"}`} onClick={() => setStatus(status == "confirmed" ? null : "confirmed")}>
+                          Confirmed
+                        </button>
+                        <button className={`btn is-pill mt-7px mr-7px ${status == "denied" ? "is-sld" : "is-out"}`} onClick={() => setStatus(status == "denied" ? null : "denied")}>
+                          Denied
+                        </button>
+                        <button className={`btn is-pill mt-7px mr-7px ${status == "canceled" ? "is-sld" : "is-out"}`} onClick={() => setStatus(status == "canceled" ? null : "canceled")}>
+                          Canceled
+                        </button>
+                        <button className={`btn is-pill mt-7px mr-7px ${status == "noshow" ? "is-sld" : "is-out"}`} onClick={() => setStatus(status == "noshow" ? null : "noshow")}>
+                          No show
+                        </button>
+                        <button className={`btn is-pill mt-7px mr-7px ${status == "arrived" ? "is-sld" : "is-out"}`} onClick={() => setStatus(status == "arrived" ? null : "arrived")}>
+                          Arrived
+                        </button>
+                        <button className={`btn is-pill mt-7px mr-7px ${status == "late" ? "is-sld" : "is-out"}`} onClick={() => setStatus(status == "late" ? null : "late")}>
+                          Late
+                        </button>
+                        <button className={`btn is-pill mt-7px mr-7px ${status == "checkdropped" ? "is-sld" : "is-out"}`} onClick={() => setStatus(status == "checkdropped" ? null : "checkdropped")}>
+                          Check dropped
+                        </button>
+                        <button className={`btn is-pill mt-7px mr-7px ${status == "finished" ? "is-sld" : "is-out"}`} onClick={() => setStatus(status == "finished" ? null : "finished")}>
+                          Finished
+                        </button>
+                      </div>
+                    </div>
+
+                    <div className="p-7px">
+                      <h5 className="font-normal">Reservation type</h5>
+                      <button className={`btn is-pill mt-7px mr-7px ${reservationtype == "online" ? "is-sld" : "is-out"}`} onClick={() => setReservationtype(reservationtype == "online" ? null : "online")}>
+                        Online
+                      </button>
+                      <button className={`btn is-pill mt-7px mr-7px ${reservationtype == "in-house" ? "is-sld" : "is-out"}`} onClick={() => setReservationtype(reservationtype == "in-house" ? null : "in-house")}>
+                        in-house
+                      </button>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="align-self-middle">No reservations</div>
               </div>
             </Tabs.TabPane>
             <Tabs.TabPane tab="Waitlist" key="2">

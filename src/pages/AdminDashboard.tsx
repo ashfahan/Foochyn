ea7a3e6c84498593b3bb05606f47b191d0bdf251
Ashfahan <me@ashfahan.com>
2020-04-28 03:33:10 +0500

    :heavy_plus_sign: "AllCuisines" and "AddCuisine" tabs

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 export default function AdminDashboard() {
-  const [view, setView] = useState<"AllUser" | "VerifiedUser" | "NonVerifiedUser" | "BannedUser" | "AllRestaurants" | "VerifiedRestaurants" | "NonVerifiedRestaurants" | "BannedRestaurants" | "AccountSetting">("AccountSetting");
+  const [view, setView] = useState<"AllUser" | "VerifiedUser" | "NonVerifiedUser" | "BannedUser" | "AllRestaurants" | "VerifiedRestaurants" | "NonVerifiedRestaurants" | "BannedRestaurants" | "AccountSetting" | "AllCuisines" | "AddCuisine">("AddCuisine");
 
   return (
     <>
@@ -59,6 +59,24 @@ export default function AdminDashboard() {
             </div>
           </div>
 
+          <div className="menu">
+            <button className="menu-trigger menu-item py-7px" onClick={(e) => (e.target as Element).classList.toggle("active")}>
+              <i className="icon is-lg ri-team-fill" />
+              Cuisine Management
+            </button>
+
+            <div className="menu ml-0">
+              <button style={{ paddingLeft: "3em" }} className={`ml-0 menu-item py-7px ${view === "AllCuisines" ? "focus" : ""}`} onClick={() => setView("AllCuisines")}>
+                <i className="icon is-lg ri-add-circle-fill" />
+                All Cuisines
+              </button>
+              <button style={{ paddingLeft: "3em" }} className={`ml-0 menu-item py-7px ${view === "AddCuisine" ? "focus" : ""}`} onClick={() => setView("AddCuisine")}>
+                <i className="icon is-lg ri-add-circle-fill" />
+                Add Cuisines
+              </button>
+            </div>
+          </div>
+
           <button className={`ml-0 menu-item py-7px ${view === "VerifiedRestaurants" ? "focus" : ""}`} onClick={() => setView("AccountSetting")}>
             <i className="icon is-lg ri-settings-line" />
             Account Setting
@@ -74,6 +92,8 @@ export default function AdminDashboard() {
           {view === "NonVerifiedRestaurants" && <NonVerifiedRestaurants />}
           {view === "BannedRestaurants" && <BannedRestaurants />}
           {view === "AccountSetting" && <AccountSetting />}
+          {view === "AllCuisines" && <AllCuisines />}
+          {view === "AddCuisine" && <AddCuisine />}
         </div>
       </div>
     </>
@@ -1090,6 +1110,132 @@ function AccountSetting() {
           <button className="btn is-sld is-valid is-pill">Update</button>
         </div>
       </div>
+    </>
+  );
+}
+
+function AllCuisines() {
+  return (
+    <>
+      <h1 className="border-bottom pb-1rem">All Cuisine</h1>
+      <div>
+        <h5>All Cuisines</h5>
+        <table className="table">
+          <thead>
+            <th>Cuisine</th>
+            <th>Description</th>
+            <th>Action</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Acai Bowls</td>
+              <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, similique hic cum laborum saepe adipisci.</td>
+              <td>
+                <a className="ml-1rem" href="#">
+                  <i className="icon ri-delete-bin-2-line"></i> Remove
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>Adobo</td>
+              <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, similique hic cum laborum saepe adipisci.</td>
+              <td>
+                <a className="ml-1rem" href="#">
+                  <i className="icon ri-delete-bin-2-line"></i> Remove
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>Agnolotti</td>
+              <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, similique hic cum laborum saepe adipisci.</td>
+              <td>
+                <a className="ml-1rem" href="#">
+                  <i className="icon ri-delete-bin-2-line"></i> Remove
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>Akashiyaki</td>
+              <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, similique hic cum laborum saepe adipisci.</td>
+              <td>
+                <a className="ml-1rem" href="#">
+                  <i className="icon ri-delete-bin-2-line"></i> Remove
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>Alfredo</td>
+              <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, similique hic cum laborum saepe adipisci.</td>
+              <td>
+                <a className="ml-1rem" href="#">
+                  <i className="icon ri-delete-bin-2-line"></i> Remove
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>Aligot</td>
+              <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, similique hic cum laborum saepe adipisci.</td>
+              <td>
+                <a className="ml-1rem" href="#">
+                  <i className="icon ri-delete-bin-2-line"></i> Remove
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>Amatriciana sauce</td>
+              <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, similique hic cum laborum saepe adipisci.</td>
+              <td>
+                <a className="ml-1rem" href="#">
+                  <i className="icon ri-delete-bin-2-line"></i> Remove
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>Anago</td>
+              <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, similique hic cum laborum saepe adipisci.</td>
+              <td>
+                <a className="ml-1rem" href="#">
+                  <i className="icon ri-delete-bin-2-line"></i> Remove
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>Anchovies</td>
+              <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, similique hic cum laborum saepe adipisci.</td>
+              <td>
+                <a className="ml-1rem" href="#">
+                  <i className="icon ri-delete-bin-2-line"></i> Remove
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>Andouille</td>
+              <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, similique hic cum laborum saepe adipisci.</td>
+              <td>
+                <a className="ml-1rem" href="#">
+                  <i className="icon ri-delete-bin-2-line"></i> Remove
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
+
+function AddCuisine() {
+  return (
+    <>
+      <h1 className="border-bottom pb-1rem">Add Cuisine</h1>
+      <form className="mb-4 grid align-middle">
+        <div className="column">
+          <input type="text" className="input" placeholder="Cuisine name" />
+        </div>
+        <button type="submit" className="btn is-sld is-primary">
+          Add
+        </button>
+      </form>
     </>
   );
 }
